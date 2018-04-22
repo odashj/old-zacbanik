0a2b916a4bed8edb01a9791028a67a75a2ea83f9
Olivia Johnson <olivia@o-j.co>
2018-04-22 19:30:59 -0400

    Added processwire site profile folders and files

---
+++ install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Zac's Portfolio Profile", 
+	'summary' => "This is a profile for Zac's portfolio website.", 
+	'screenshot' => "screen_shot_2018-04-22_at_7_24_32_pm.png"
+	);
